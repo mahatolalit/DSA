f447ea61d1f0b8689877b2b891c5da716a1cb2bb
mahatolalit <lalitmahato20051024@gmail.com>
2025-08-17 08:11:40 +0530

    Add ListIterator.java

---
+++ Java Collections/ListIterator.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListIterator {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        System.out.println(list);
+
+        Iterator<Integer> iter = list.iterator();
+
+        while (iter.hasNext()) {
+            System.out.println("Element " + iter.next());
+        }
+    }
+}
