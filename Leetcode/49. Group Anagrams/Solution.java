76a36d058d53f0da61dbfebdd412a057ad536eb1
mahatolalit <lalitmahato20051024@gmail.com>
2025-09-02 19:10:22 +0530

    Group Anagrams

---
+++ Leetcode/49. Group Anagrams/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            int[] count = new int[26]; 
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder keyBuilder = new StringBuilder();
+            for (int freq : count) {
+                keyBuilder.append(freq).append('#'); 
+            }
+            String key = keyBuilder.toString();
+
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
