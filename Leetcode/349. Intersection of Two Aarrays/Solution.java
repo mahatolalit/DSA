951b179b31425bbcb4a1d0cd7fa91c4c362ef37e
mahatolalit <lalitmahato20051024@gmail.com>
2025-09-09 07:20:29 +0530

    349. Intersection of Two Arrays
    -> Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.

---
+++ Leetcode/349. Intersection of Two Aarrays/Solution.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        for (int num: nums2) {
+            if (set1.contains(num))
+            set2.add(num);
+        }
+
+        int[] result = new int[set2.size()];
+        int i = 0;
+        for (int num : set2) {
+            result[i++] = num;
+        }
+
+        return result;
+
+    }
+}
